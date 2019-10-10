8ee19ab9eb3da468da5c11afed0d4f9b9b05b61c
Mik3y-F <mike18farad@gmail.com>
2019-10-10 20:32:09 +0300

    Played with columns

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red[600],
+        title: Text('Mike is awesome'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.cyan,
+            child: Center(
+              child: Text(
+                "One",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: "HeptaSlab",
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.pinkAccent,
+            child: Center(
+              child: Text(
+                "Two",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: "HeptaSlab",
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.amber,
+            child: Center(
+              child: Text(
+                "Three",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: "HeptaSlab",
+                ),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                child: Text(
+                  "Hey bro",
+                  style: TextStyle(
+                    fontFamily: "HeptaSlab",
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              Container(
+                child: Text(
+                  "My Name is Mikey",
+                  style: TextStyle(
+                    fontFamily: "HeptaSlab",
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('Click'),
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
