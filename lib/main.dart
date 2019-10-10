6db9cfbc664c3ffaf945c2fe6c99360c664b1e28
Mik3y-F <mike18farad@gmail.com>
2019-10-10 16:53:57 +0300

    Added image to layout

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red[600],
+        title: Text('Mike is awesome'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Image.asset('assets/baby.jpg'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('Click'),
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
